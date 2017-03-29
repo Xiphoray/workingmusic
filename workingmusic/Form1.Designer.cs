0e4358f078a4cc8a00880e3e038c3df1c9163b32
xiphoray <351014842@qq.com>
2017-03-29 20:51:25 +0800

    Version 1.0 can use

--- workingmusic/Form1.Designer.cs
+++ workingmusic/Form1.Designer.cs
@@ -28,12 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.people = new AxWMPLib.AxWindowsMediaPlayer();
+            this.rain = new AxWMPLib.AxWindowsMediaPlayer();
+            this.thunder = new AxWMPLib.AxWindowsMediaPlayer();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fire = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.people)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thunder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fire)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.button1.Location = new System.Drawing.Point(205, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "工作啦";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(12, 75);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // people
+            // 
+            this.people.Enabled = true;
+            this.people.Location = new System.Drawing.Point(200, 232);
+            this.people.Name = "people";
+            this.people.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("people.OcxState")));
+            this.people.Size = new System.Drawing.Size(53, 42);
+            this.people.TabIndex = 3;
+            this.people.Visible = false;
+            // 
+            // rain
+            // 
+            this.rain.Enabled = true;
+            this.rain.Location = new System.Drawing.Point(259, 232);
+            this.rain.Name = "rain";
+            this.rain.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rain.OcxState")));
+            this.rain.Size = new System.Drawing.Size(59, 41);
+            this.rain.TabIndex = 4;
+            this.rain.Visible = false;
+            // 
+            // thunder
+            // 
+            this.thunder.Enabled = true;
+            this.thunder.Location = new System.Drawing.Point(374, 177);
+            this.thunder.Name = "thunder";
+            this.thunder.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("thunder.OcxState")));
+            this.thunder.Size = new System.Drawing.Size(98, 97);
+            this.thunder.TabIndex = 5;
+            this.thunder.Visible = false;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(127, 75);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Visible = false;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(347, 75);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 8;
+            this.trackBar3.Visible = false;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.LargeChange = 1;
+            this.trackBar4.Location = new System.Drawing.Point(237, 75);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 9;
+            this.trackBar4.Visible = false;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label1.Location = new System.Drawing.Point(43, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "fire";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label2.Location = new System.Drawing.Point(161, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "people";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label3.Location = new System.Drawing.Point(268, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "rain";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(381, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "thunder";
+            this.label4.Visible = false;
+            // 
+            // fire
+            // 
+            this.fire.Enabled = true;
+            this.fire.Location = new System.Drawing.Point(7, 177);
+            this.fire.Name = "fire";
+            this.fire.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("fire.OcxState")));
+            this.fire.Size = new System.Drawing.Size(123, 95);
+            this.fire.TabIndex = 14;
+            this.fire.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "去玩啦";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 270);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.fire);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.thunder);
+            this.Controls.Add(this.rain);
+            this.Controls.Add(this.people);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.people)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thunder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fire)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private AxWMPLib.AxWindowsMediaPlayer people;
+        private AxWMPLib.AxWindowsMediaPlayer rain;
+        private AxWMPLib.AxWindowsMediaPlayer thunder;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private AxWMPLib.AxWindowsMediaPlayer fire;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- workingmusic/Form1.cs
+++ workingmusic/Form1.cs
@@ -15,6 +15,100 @@ namespace workingmusic
         public Form1()
         {
             InitializeComponent();
+            init();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            work_show();
+            button1.Hide();
+            play();
+        }
+        public void play()
+        {
+            fire.Ctlcontrols.play();
+            people.Ctlcontrols.play();
+            rain.Ctlcontrols.play();
+            thunder.Ctlcontrols.play();
+
+        }
+        public void display()
+        {
+            fire.Ctlcontrols.stop();
+            people.Ctlcontrols.stop();
+            rain.Ctlcontrols.stop();
+            thunder.Ctlcontrols.stop();
+
+        }
+        public void init()
+        {
+            fire.URL = Define.MisPath + "fire.mp4";
+            people.URL = Define.MisPath + "people.mp4";
+            rain.URL = Define.MisPath + "rain.mp4";
+            thunder.URL = Define.MisPath + "thunder.mp4";
+            fire.settings.setMode("loop", true);
+            people.settings.setMode("loop", true);
+            rain.settings.setMode("loop", true);
+            thunder.settings.setMode("loop", true);
+        }
+        
+        public void work_show()
+        {
+            trackBar1.Show();
+            trackBar2.Show();
+            trackBar3.Show();
+            trackBar4.Show();
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            label4.Show();
+            button2.Show();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            thunder.settings.volume = trackBar3.Value;
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            fire.settings.volume = trackBar1.Value;
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            rain.settings.volume = trackBar4.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            people.settings.volume = trackBar2.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Play_Hide();
+            button1.Show();
+            display();
+        }
+
+        public void Play_Hide()
+        {
+            trackBar1.Hide();
+            trackBar2.Hide();
+            trackBar3.Hide();
+            trackBar4.Hide();
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            button2.Hide();
+        }
+    }
+    public static class Define
+    {
+        public static string MisPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"music\";
+
     }
 }
